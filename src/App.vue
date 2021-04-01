feaca25170b5e611fdd818dbae72ffda213500ef
Ankur Chauhan <ankur@readybytes.in>
2021-04-01 15:40:31 +0530

    final

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-main app>
-      <HelloWorld/>
+      <Weather/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Weather from './components/WeatherComponent';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Weather,
   },
 
   data: () => ({
@@ -23,12 +23,12 @@ export default {
 </script>
 
 <style>
-  /* #app {
-  background: url('https://ohlaladani.com.br/wp-content/uploads/wallpaper-OHLALADANI_DESKTOP_WALLPAPERS_AVENTURA-2.jpg')
+  #app {
+  background: url('https://images.pexels.com/photos/125457/pexels-photo-125457.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
     no-repeat center center fixed !important;
   background-size: cover;
   transition: 0.5s;
-  } */
+  }
   main{
     background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
   }
--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -12,22 +12,19 @@
     </v-col>
     <div v-if="weather.weather && weather.weather.length">
     <v-row >
-      <!--  -->
       <v-col class="">
         <v-card class="transparent ml-10" elevation="0">
           <v-list-item class="pa-0 ma-0">
             <v-list-item-content>
-              <!-- <v-list-item-icon> -->
-                <!-- <v-icon color="white" size=100>mdi-cloud</v-icon> -->
                 <div>
-                  <v-img contain class="ml-n10" height="200" width="200" :src="`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`" />
+                  <v-img contain class="ml-n8" height="200" width="200" :src="`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`" />
                 </div>
                 
-              <!-- </v-list-item-icon> -->
               <v-list-item-title class="white--text display-3 font-weight-medium">
                 {{ weather.weather[0].main}}
               </v-list-item-title>
               <v-list-item-title class="white--text headline">{{ weather.name }}, {{ weather.sys.country }}</v-list-item-title>
+              <v-list-item-title class="white--text headline">{{ days[new Date(weather.dt * 1000).getDay()] }}, {{ new Date(weather.dt * 1000).getDate() }} {{ months[new Date(weather.dt * 1000).getMonth()] }} </v-list-item-title>
               <v-list-item-title class="white--text display-3 font-weight-black my-5">{{ Math.round(weather.main.temp - 274) }}°C</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
@@ -48,10 +45,9 @@
       </v-col>
     </v-row>
 
-    <v-sheet :style="css" class="justify-center transparent mt-15 mx-2" elevation="0">
+    <v-sheet class="justify-center transparent mt-15 mx-2" elevation="0">
       <v-slide-group
         v-model="model"
-        class=""
         show-arrows
         dark
       >
@@ -69,7 +65,10 @@
             {{days[new Date(daily.dt * 1000).getDay()]}}
           </v-card-title>
           <v-img contain class="center " height="100" width="100" :src="`http://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`" />
-          <v-card-title class="white--text font-weight-bold justify-center">{{ Math.round(daily.temp.max - 274) }}°C | {{ Math.round(daily.temp.min - 274) }}°C</v-card-title>
+          <v-card-title class="white--text font-weight-bold justify-center">
+            <span class="display-1 font-weight-bold">{{ Math.round(daily.temp.max - 274) }}°C | </span>
+            <span class="title font-weight-bold grey--text"> {{ Math.round(daily.temp.min - 274) }}°C</span>
+            </v-card-title>
           
           </v-card>
         </v-slide-item>
@@ -100,42 +99,27 @@
       return {
         model: null,
         city: 'Bhilwara',
-        backgroundImage: 'red',
-        
         api_key: '5d58c07735d7fe6727a84e9fdbcbd119',
         weather: {},
         otherData: [],
         dailyForecast: {},
         days:['Sunday','Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday'],
+        months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
         day: null,
       }
       
     },
     mounted(){
       this.getWeatherData();
-      
-    },
-    computed:{
-      css(){
-        return{
-          '--background': this.backgroundImage
-        }
-      }
-      
     },
     methods: {
       async getWeatherData(){
         await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.api_key}`).then(res => {
-          // console.log(res)
           return res.json();
         }).then(result => {
-          // console.log(result)
           this.weather = result
-          // if(this.weather && this.weather.length && this.weather.weather[0].icon)
-          //   this.background = this.weather.weather[0].icon + 'png'
  
         });
-        console.log(this.weather);
         this.otherData = [
           {
             icon: "mdi-weather-hail",
@@ -152,37 +136,17 @@
             title: 'Wind Speed',
             statistics: this.weather.wind.speed + ' km/h'
           },
-          // {
-          //   title: 'Sunrise',
-          //   statistics: moment(this.weather.sys.sunrise)
-          // },
-          // {
-          //   title: 'Sunset',
-          //   statistics: this.weather.sys.sunset
-          // },
           ];
           fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.weather.coord.lat}&lon=${this.weather.coord.lon}&exclude={minutely}&appid=${this.api_key}`).then(data=>{
             return data.json();
           }).then(result=>{
-            console.log(result.daily);
             this.dailyForecast = result.daily;
             let date = new Date(result.daily[0].dt * 1000)
-            // console.log(date.getDay())
             this.day = date.getDay()
           })
-          // this.day = this.dailyForecast
           setInterval(function(){ window.location.reload() }, 300000);
       }
       
     },
   }
 </script>
-<style>
-#app {
-  background: url("https://images.pexels.com/photos/912364/pexels-photo-912364.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-    no-repeat center center fixed !important;
-    color: red;
-  background-size: cover;
-  transition: 0.5s;
-  } 
-</style>
